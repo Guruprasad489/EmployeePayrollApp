bc6c3ea6af4169c38570cdcf2f251ada99ff1489
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-03-20 22:17:20 +0530

    [Guru] Populate emp data onsubmit form

--- JS/EmpPayrollRegistration.js
+++ JS/EmpPayrollRegistration.js
@@ -1,7 +1,45 @@
-//Dynamic salary range based on user input
-const salary = document.querySelector('#salary');
-const output = document.querySelector('.salary-output');
-output.textContent = salary.value;
-salary.addEventListener('input', function(){
-    output.textContent = salary.value;
-})
+// //Dynamic salary range based on user input
+// const salary = document.querySelector('#salary');
+// const output = document.querySelector('.salary-output');
+// output.textContent = salary.value;
+// salary.addEventListener('input', function(){
+//     output.textContent = salary.value;
+// })
+
+
+//Javascript to add set event listeners on document loading(UC10)
+window.addEventListener('DOMContentLoaded', (event) => {
+    //Setting the name to the employee object for validation
+    const name = document.querySelector('#empName');
+    const nameError = document.querySelector('#errorName');
+    name.addEventListener('input', () => {
+        if (name.value.length == 0) {
+            nameError.textContent = '';
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).empName = name.value;
+            nameError.textContent = '';
+        } catch (e) {
+            nameError.textContent = e;
+        }
+    });
+    //Setting the salary output by checking the salary value(UC10)
+    const salary = document.querySelector('#salary');
+    const outputSal = document.querySelector('#salaryOutput');
+    outputSal.textContent = salary.value;
+    salary.addEventListener('input', () => {
+        outputSal.textContent = salary.value;
+    });
+    //Setting the date to the employee object for validation(UC10)
+    const date = document.querySelector('#date');
+    const errorDate = document.querySelector('#errorDate');
+    date.addEventListener('input', function() {
+        try {
+            (new EmployeePayrollData()).startDate = new Date(date);
+            errorDate.textContent = "";
+        } catch (e) {
+            errorDate.textContent = e;
+        }
+    });
+}); 
